006d97b9e8df7ab3256b380fa59193e2143c0935
gwanggyu park <pgg961@gmail.com>
2021-01-11 15:24:40 +0900

    edit desgins

--- macro_list.py
+++ macro_list.py
@@ -6,7 +6,7 @@ import openpyxl
 # connection UI file to Python file. UI file and Python File must be in the same directory
 form_class = uic.loadUiType("macro_list.ui")[0]
 
-# -----------------------make two lists of postback macro-----------------------------
+# -----------------------make lists of postback macro-----------------------------
 # "/Users/gwanggyupark/Documents/postback_macro.xlsx" -> file directory for Mac
 wb = openpyxl.load_workbook("postback_macro.xlsx", data_only=True)
 
@@ -17,6 +17,7 @@ count_attribution = 0
 for row in ws_attribution:
     if not all([cell.value is None for cell in row]):
         count_attribution += 1
+print(count_attribution)
 # cells
 cells_attribution = ws_attribution['A1':'A%d' % count_attribution]
 cells_attribution_description = ws_attribution['B1':'B%d' % count_attribution]
@@ -49,6 +50,7 @@ count_event = 0
 for row in ws_event:
     if not all([cell.value is None for cell in row]):
         count_event += 1
+print(count_event)
 # cells
 cells_event = ws_event['A1':'A%d' % count_event]
 cells_event_description = ws_event['B1':'B%d' % count_event]
@@ -72,6 +74,8 @@ for row in cells_event_example:
         event_macro_list_example.append(cell.value)
 
 
+# -------------------------------------------------------------------------
+
 # window class
 class WindowClass(QMainWindow, form_class):
     def __init__(self):
@@ -81,43 +85,53 @@ class WindowClass(QMainWindow, form_class):
         i = 0
         # default value
         while i < len(attribution_macro_list):
-            print(attribution_macro_list[i])
+            # print(attribution_macro_list[i])
             self.macroList.addItem(attribution_macro_list[i])
             i = i + 1
 
         self.radioButton.clicked.connect(self.checkRadio)
-        self.radioButton_2.clicked.connect(self.checkRadio)
-        self.macroList.clicked.connect(self.listClickFunction)
+        self.radioButton2.clicked.connect(self.checkRadio)
+        self.macroList.itemClicked.connect(self.listClickFunction)
 
     # function when radio button is clicked
     def checkRadio(self):
         self.macroList.clear()
-        print("either of radio buttons is clicked")
-        if self.radioButton.isChecked:
+        if self.radioButton.isChecked():
             i = 0
             print("radioButton checked")
             while i < len(attribution_macro_list):
                 self.macroList.addItem(attribution_macro_list[i])
                 i = i + 1
-        elif self.radioButton_2.isChecked:
+        elif self.radioButton2.isChecked():
             i = 0
             print("radioButton2 checked")
             while i < len(event_macro_list):
                 self.macroList.addItem(event_macro_list[i])
                 i = i + 1
 
+    # def checkRadio2(self):
+    #     self.macroList.clear()
+    #     i = 0
+    #     print("radioButton2 checked")
+    #     while i < len(event_macro_list):
+    #         self.macroList.addItem(event_macro_list[i])
+    #         i = i + 1
+
     # function when a item on the list is clicked
     def listClickFunction(self):
         self.macroDescription.clear()
         index = self.macroList.currentRow()
+        print(index)
         if self.radioButton.isChecked:
+            print("radioButton clicked : %d" + self.radioButton.isChecked)
             description = attribution_macro_list_description[index]
             example = attribution_macro_list_example[index]
             self.macroDescription.append("%s \n\n%s" % (description, example))
-        elif self.radioButton_2.isChecked:
-            description = event_macro_list_description[index]
-            example = event_macro_list_example[index]
-            self.macroDescription.append("%s \n\n%s" % (description, example))
+        elif self.radioButton2.isChecked:
+            print("radioButton2 clicked : %d" % index)
+            description2 = event_macro_list_description[index]
+            example2 = event_macro_list_example[index]
+            self.macroDescription.append("%s \n\n%s" % (description2, example2))
 
 
 # execute this file
